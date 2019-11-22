290285d777339d175c75bab51a01ed56f8e916e1
Tommaso <tgarutti@gmail.com>
2019-11-22 17:55:32 +0100

    Moving window so close

--- movingWindow.m
+++ movingWindow.m
@@ -1,6 +1,6 @@
 function [ output ] = movingWindow(w, yields, lambda, tau, steps_ahead, FUN )
 %MOVINGWINDOW
-
+%Initialize variables
 [M,T] = size(yields);
 S = numel(steps_ahead);
 forecasts = cell(S,1);
@@ -8,6 +8,7 @@ for s=1:S
     forecasts{s} = zeros(M,T-w);
 end
 
+%Forecast yields over moving window
 for i=1:(T-w)
     window = i:(i+w-1);
     yields_window = yields(:, window);
@@ -18,5 +19,18 @@ for i=1:(T-w)
     end
 end
 
-output = forecasts;
+%Compute RMSFE
+RMSFE = zeros(M,S);
+output = cell(S+1,1);
+
+for s=1:S
+    step = steps_ahead(s);
+    actuals = yields(:,(w+step):end);
+    forecast_s = forecasts{s}(:,1:(end-step));
+    RMSFE(:,s) = mean(sqrt((actuals-forecast_s).^2),2);
+    output{s} = forecasts{s};
+end
+
+output{S+1} = RMSFE;
+
 end
